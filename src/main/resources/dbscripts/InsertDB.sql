0e83d1fc37a4882594e30d5a6ed50a81b6c36be2
vasanthrao <vasantharao.vanjarapu@gmail.com>
2025-04-01 22:13:09 +0530

    Added user insert script

--- src/main/resources/dbscripts/InsertDB.sql
+++ src/main/resources/dbscripts/InsertDB.sql
@@ -2,6 +2,12 @@
 -- insert into agency (agency_id, agency_name) values (1,'ALEAP'),(2,'WeHub'),(3,'TIHCL'),(4,'Commissionarate of Industries'),(5,'GT');
 insert into agency (agency_id, agency_name) values (1,'ALEAP'),(2,'WeHub'),(3,'TIHCL');
 
+-- Insert users
+INSERT INTO user (user_id,address,attempts,created_on,email,first_name,gender,last_name,mobile_no,password,status,updated_on,user_role,agency_id) VALUES ('admin@gmail.com',NULL,NULL,'2025-04-01 21:39:32.260000','admin@gmail.com','Murthy','Male','NV',9948022300,'Password@123',NULL,NULL,'ADMIN',NULL);
+INSERT INTO user (user_id,address,attempts,created_on,email,first_name,gender,last_name,mobile_no,password,status,updated_on,user_role,agency_id) VALUES ('aleap@gmail.com',NULL,NULL,'2025-04-01 21:39:43.193000','aleap@gmail.com','Aleap','Male','E',9948022301,'Password@123',NULL,NULL,'AGENCY_MANAGER',1);
+INSERT INTO user (user_id,address,attempts,created_on,email,first_name,gender,last_name,mobile_no,password,status,updated_on,user_role,agency_id) VALUES ('tihcl@gmail.com',NULL,NULL,'2025-04-01 21:39:15.296000','tihcl@gmail.com','TIHCL','Male','E',9948022303,'Password@123',NULL,NULL,'AGENCY_MANAGER',3);
+INSERT INTO user (user_id,address,attempts,created_on,email,first_name,gender,last_name,mobile_no,password,status,updated_on,user_role,agency_id) VALUES ('WeHub@gmail.com',NULL,NULL,'2025-04-01 21:39:56.053000','WeHub@gmail.com','WeHub','Male','W',9948022302,'Password@123',NULL,NULL,'AGENCY_MANAGER',2);
+
 -- Insert program outcome tables
 insert into program_outcome_tables (outcome_table_id, outcome_table_display_name, outcome_table_name) values
 (1, 'ONDC Registration', 'ONDCRegistration'),
@@ -14,9 +20,9 @@ insert into district(district_id,district_name) values (1,'Adilabad'),(2,'Hydera
 insert into mandal(mandal_id,mandal_name,district) values(1,'Adilabad(Rural)',1),(2,'Adilabad(urbarn)',1);
 
 -- Insert Activity & Sub Activity table script
-insert into activity(activity_id,activity_name) values(1,'Sample Activity1');
+insert into activity(activity_id,activity_name,agency) values(1,'Sample Activity1',1);
 insert into sub_activity(sub_activity_id,sub_activity_name) values(1,'Sample Sub Activity1');
-insert into activity(activity_id,activity_name) values(2,'Sample Activity2');
+insert into activity(activity_id,activity_name,agency) values(2,'Sample Activity2',1);
 insert into sub_activity(sub_activity_id,sub_activity_name) values(2,'Sample Sub Activity2');
 insert into activity_sub_activity(activity_id,sub_activity_id) values(1,1);
 insert into activity_sub_activity(activity_id,sub_activity_id) values(2,2);
