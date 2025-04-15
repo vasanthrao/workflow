1076cd8fd3992c6daeb48b4c9533c5586f85b096
peddinasameer <peddinasameer@gmail.com>
2025-04-15 10:13:59 +0530

    adding DB sreapts

---
+++ src/main/resources/dbscripts/InsertDB_Program_type.sql
@@ -0,0 +1,11 @@
+-- insert screapt for program_type
+insert into program_type (program_type_id,program_type,agency_id) values(1,'Awareness Program',1),(2,'Training',1);
+insert into program_type (program_type_id,program_type,agency_id) values(3,'Documentation Workshop',2),(4,'Skill Development Training',2),(5,'Workshop',2),(6,'Conference',2);
+insert into program_type (program_type_id,program_type,agency_id) values(7,'Workshop',3);
+insert into program_type (program_type_id,program_type,agency_id) values(8,'Awareness Program',4),(9,'RoundTable Discussion',4),(10,'Startup Grand Challenge',4);
+insert into program_type (program_type_id,program_type,agency_id) values(11,'Workshop',5),(12,'Awareness Program',5);
+insert into program_type (program_type_id,program_type,agency_id) values(13,'Workshop',6),(14,'Awareness Program',6);
+insert into program_type (program_type_id,program_type,agency_id) values(15,'ToT',7),(16,'Awareness Program',7),(17,'Skill Development Program',7),(18,'Training',7),(19,'Symposisum',7);
+insert into program_type (program_type_id,program_type,agency_id) values(20,'Workshop',8),(21,'Event',8),(22,'Training',8),(23,' ToT',8);
+insert into program_type (program_type_id,program_type,agency_id) values(24,'ESDP Training',9),(25,'Awareness Program',9)
+
